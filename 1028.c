db4d2c61ba5b27681bcd1cbf6753e5596cf504b0
Saurabh Goyal <saurabh.2561@gmail.com>
2014-08-08 10:19:26 +0530

    Create 1028.c
    Hubulullu

---
+++ 1028.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int t=0;
+	scanf("%d",&t);
+	while(t--){
+		long long int a=0;
+		scanf("%lld",&a);
+		int first=0;
+		scanf("%d",&first);
+		first==0?printf("Airborne wins.\n"):printf("Pagfloyd wins.\n");
+	}
+	return 0;
+}
